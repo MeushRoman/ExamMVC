a10da0757706cd354497a62ef950ae22a727ef4b
Roman <meush.roman@gmail.com>
2022-05-31 16:34:18 +0600

    add

---
+++ ExamProj/Entities/Models/Answer - Копировать.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamProj.Entities.Models
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string AText { get; set; }
+        public Question Question { get; set; } 
+    }
+}
