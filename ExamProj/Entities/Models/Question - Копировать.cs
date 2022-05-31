f884b21602339942f69811d2229b55798c00fd13
Roman <amaii.sakura@gmail.com>
2022-05-31 16:59:54 +0600

    tttt

---
+++ ExamProj/Entities/Models/Question - Копировать.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamProj.Entities.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string QText { get; set; }
+        public int RealAnswerId { get; set; }  
+        public Answer Answer { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+}
